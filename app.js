d0dd5f24784f517e5c26a3fcd0979e3da12ba72c
jxhnkndl <jkroyston@gmail.com>
2021-01-23 20:43:14 -0500

    Add contact section and adjust overall page styling

--- app.js
+++ app.js
@@ -1,40 +1,5 @@
 $(document).ready(function() {
 
-  // Project data object
-  const projects = [
-    {
-      title: "Foo Bar Finder",
-      img: {
-        url: "./assets/img/foo-bar-finder.png",
-        alt: "Foo Bar Finder application screen shot"
-      },
-      technologies: "UI / UX / Bootstrap / JavaScript / jQuery",
-      description: "The Foo Bar Finder is a double-sided application that allows a user to search for and save new recipes and cocktails by keyword or category. The application uses alternate color schemes for the recipe and drink sides."
-    },
-    {
-      title: "Workday Scheduler",
-      img: {
-        url: "./assets/img/workday-scheduler.png",
-        alt: "Workday Scheduler application screen shot"
-      },
-      technologies: "JavaScript / jQuery / Moment.js",
-      description: "The Workday Scheduler is a dynamic planner application allowing users to input and save events spanning from 9 AM to 5 PM. Timeblocks auto-update their background colors in relation to the current time."
-    },
-    {
-      title: "Weather Dashboard",
-      img: {
-        url: "./assets/img/weather-dashboard.png",
-        alt: "Weather Dashboard application screen shot"
-      },
-      technologies: "UI / UX / Bootstrap / JavaScript / jQuery / Day.js",
-      description: "The Weather Dashboard allows users to input a city and retrieve both the city's current weather and five-day forecast. The application also saves previously search cities for quick and easy future reference."
-    }
-  ];
-
-  // Global variables
-  let currentIndex = 0;
-  let lastIndex = projects.length;
-
   init();
 
   // Initialize animate on scroll library
@@ -55,22 +20,12 @@ $(document).ready(function() {
       delay: 0,
       duration: 400,
       easing: 'ease-in-out-quad',
-      once: false,
+      once: true,
       mirror: false,
       anchorPlacement: 'top-bottom'
     });
-
-    showProject();
   }
 
-  // Display project data
-  function showProject() {
-    $("#project-title").text(projects[currentIndex].title);
-    $("#project-img").attr("src", projects[currentIndex].img.url);
-    $("#project-img").attr("alt", projects[currentIndex].img.alt);
-    $("#project-tech").text(projects[currentIndex].technologies);
-    $("#project-description").text(projects[currentIndex].description);
-  }
 
   // Event Listener: Create smooth scroll 
   $('a[href^="#"').on("click", function(e) {
@@ -83,17 +38,18 @@ $(document).ready(function() {
     }, 500);
   });
 
-  // Event Listener: Press button down
-  $(".btn").on("mousedown", function() {
-    $(this).css({
-      transform: "translateY(3px)"
-    })
-  });
-
-  // Event Listener: Let go of button
-  $(".btn").on("mouseup", function() {
-    $(this).css({
-      transform: "translateY(-3px)"
-    });
+  // Event Listener: Button behavior
+  $(".btn").on({
+    mousedown: function() {
+      $(this).css({
+        transform: "translateY(0)",
+        transform: "translateY(2px)"
+      });
+    },
+    mouseup: function() {
+      $(this).css({
+        transform: "translateY(-2px)"
+      });
+    }
   });
 });
